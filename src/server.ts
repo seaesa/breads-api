36dd0a3eef029b2ac6a6d4a12f21774e06a93bd4
ngoc hai <hairipi100@gmail.com>
2024-09-10 14:21:01 +0700

    upgrade tool

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import cors from 'cors';
 import router from './routes/routes';
 const app = express();

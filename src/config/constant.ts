a706bfd5f0650f1182c1681df35591a74f8756bb
ngoc hai <hairipi100@gmail.com>
2024-09-11 23:33:53 +0700

    configuration env

---
+++ src/config/constant.ts
@@ -0,0 +1,5 @@
+export const env = {
+	DATABASE_URI: process.env.DATABASE_URI,
+	JWT_SECRET_TOKEN: process.env.JWT_SECRET_TOKEN,
+	JWT_REFRESH_TOKEN: process.env.JWT_REFRESH_TOKEN,
+};
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,22 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
 import router from './routes/routes';
+import dotenv from 'dotenv';
+import path from 'path';
+import morgan from 'morgan';
 const app = express();
+
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => res.json({ code: 200 }));
-
 app.use(router);
 
-app.listen(3000, () => console.log(`server running on http://localhost:3000`));
-
+if (process.env.NODE_ENV === 'development') {
+	dotenv.config({
+		path: path.resolve(__dirname, '.env.development.local'),
+	});
+	app.use(morgan('dev'));
+	app.listen(3000, () => console.log(`server running on http://localhost:3000`));
+}
+// erport for deploy to vercel
 export default app;

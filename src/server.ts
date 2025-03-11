53ec3b786fbb4a214a763cc366b8d864734d4b5a
ngoc hai <hairipi100@gmail.com>
2025-03-11 16:33:01 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,10 @@ import dotenv from 'dotenv';
 import path from 'path';
 import morgan from 'morgan';
 const app = express();
-
+import crypto from 'crypto';
 app.use(cors());
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => res.json({ code: 200 }));
 app.use(router);
@@ -19,5 +21,29 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 	app.listen(3000, () => console.log(`server running on http://localhost:3000`));
 }
+
+const hashPassword = (password: string) => crypto.createHash('md5').update(password).digest('hex');
+const fakeData = [
+	{
+		username: 'hai',
+		email: 'hairipi100@gmail.com',
+		displayName: 'Ngọc Hải',
+		password: hashPassword('cocainit'),
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+];
+app.post('/api/login', (req, res) => {
+	const { email, password } = req.body;
+	const user = fakeData.find((data) => data.email === email);
+	if (!user) return res.json({ code: 404, message: 'user not found!' });
+	else {
+		const matchedPassword = hashPassword(password);
+		if (user.password !== matchedPassword)
+			return res.json({ code: 401, message: 'password not matched!' });
+		else return res.json({ code: 200, message: 'login successfully' });
+	}
+});
+
 // erport for deploy to vercel
 export default app;
